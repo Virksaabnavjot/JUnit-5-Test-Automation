49eb16f057026429121b3dbcad24a6ebc67acadc
virksaabnavjot <virksaabnavjot@gmail.com>
2017-10-22 00:17:55 +0100

    Let Automation testing / Unit Testing begin using JUnit 5

---
+++ src/com/navjot/junit/PriorityQueueExample.java
@@ -0,0 +1,15 @@
+package com.navjot.junit;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+import static nsv.custom.utilities.Print.*;
+
+public class PriorityQueueExample {
+
+	Queue<String> priorityQ = new PriorityQueue();
+	public static void main(String[] args) {
+		
+
+	}
+
+}
