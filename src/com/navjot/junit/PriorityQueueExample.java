048e58bad8689cb698fc79b92eefe7bf68284a9d
virksaabnavjot <virksaabnavjot@gmail.com>
2017-10-22 00:41:41 +0100

    Passing Test: After adding elements

--- src/com/navjot/junit/PriorityQueueExample.java
+++ src/com/navjot/junit/PriorityQueueExample.java
@@ -1,5 +1,7 @@
 package com.navjot.junit;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import static nsv.custom.utilities.Print.*;
@@ -7,6 +9,21 @@ import static nsv.custom.utilities.Print.*;
 public class PriorityQueueExample {
 
 	private static Queue<String> priorityQueue = new PriorityQueue();
+	
+	public static void main(String[] args) {
+		addElementsToThePriorityQueue();
+		println(priorityQueue);
+
+	}
+
+	public static void addElementsToThePriorityQueue() {
+		priorityQueue.add("Apple");
+		priorityQueue.add("Banana");
+		priorityQueue.add("Man");
+		priorityQueue.add("Cat");
+		priorityQueue.add("Zebra");
+		priorityQueue.add("Horse");
+	}
 	
 	public static Queue<String> getPriorityQueue() {
 		return priorityQueue;
@@ -16,12 +33,4 @@ public class PriorityQueueExample {
 		PriorityQueueExample.priorityQueue = priorityQueue;
 	}
 
-	public static void main(String[] args) {
-		println(priorityQueue);
-
-	}
-
-	public static void addElementsToThePriorityQueue() {
-		
-	}
 }
