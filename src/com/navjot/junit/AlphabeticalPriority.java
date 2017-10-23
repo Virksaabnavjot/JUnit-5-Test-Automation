181bc9130d899dc9285c5025e1606cb1f88a17b7
virksaabnavjot <virksaabnavjot@gmail.com>
2017-10-23 01:19:02 +0100

    Minor change

--- src/com/navjot/junit/AlphabeticalPriority.java
+++ src/com/navjot/junit/AlphabeticalPriority.java
@@ -6,7 +6,7 @@ public class AlphabeticalPriority implements Comparator<String>{
 
 	@Override
 	public int compare(String s1, String s2) {
-		return s1.compareTo(s2);
+		return s1.compareTo(s2); //A to Z
 	}
 
 }
